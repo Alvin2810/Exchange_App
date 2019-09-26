a975f03860cd26b2a844325b78183f85c7be7a63
Alvin Mammoottil <monsialvin@gmail.com>
2019-09-26 22:29:25 +0930

    Adding javascript function to find total money spent

--- website.js
+++ website.js
@@ -2,7 +2,8 @@ function add_Expense(){
   var button1 = document.getElementById('button1');
   button1.addEventListener('click', saveExpense, false);
   display();
-  
+  sum();
+
 }
 
 function saveExpense(){
@@ -37,6 +38,21 @@ function display(){
 
   }
 }
+
+function sum(){
+    if(sessionStorage.length > 0){
+        var sum = 0;
+        for (var i=0, len=sessionStorage.length; i<len; i++){
+            var key = sessionStorage.key(i);
+            var val = sessionStorage.getItem(key);
+            var valu = val.split("*");
+            sum += parseInt(valu[0]);
+            total.innerHTML="- $ "+sum;
+
+          }
+      }
+};
+total.innerHTML="- $ 0";
 
 
 window.addEventListener('load', add_Expense, false)
